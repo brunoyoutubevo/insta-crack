ea9640a07872a1e16f680c1cd3a4c360faa05157
brunoyoutubevo <71612839+brunoyoutubevo@users.noreply.github.com>
2020-09-20 23:42:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Author: github.com/thelinuxchoice
-#Instagram: @thelinuxchoice
+#Instagram: @aldraberto.provador
 trap 'echo exiting cleanly...; exit 1;' SIGINT SIGTSTP
 
 checkroot() {
